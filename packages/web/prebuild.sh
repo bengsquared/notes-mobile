78200b9eade964a0a44cb8f93013842360746e3c
Ben Graney Green <bengg@mac.com>
2025-05-27 23:14:24 -0700

    removing root vercel config

---
+++ packages/web/prebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs before the build to copy any needed external files
+
+# Example: If you need the shared package later
+# cp -r ../shared ./shared-temp
+
+# For now, we don't need anything from outside packages/web
+echo "Prebuild complete"
